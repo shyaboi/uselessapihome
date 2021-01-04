722bbba2cefd7f7da5fe5edaa674ddcf504e43d3
Shyaboi <ian.shane.sears@gmail.com>
2021-01-04 01:24:43 -0700

    initial commit, home page with jumbotron, and dropdown with links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-function App() {
+
+const Example = (props) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen(prevState => !prevState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Jumbotron fluid id="jumbo">
+        <Container fluid>
+          <h1 className="display-3">UselessAPI.com</h1>
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+          <p className="lead">Welcome to uselessapi.com</p>
+          <br></br>
+          <p>A collection of APIs</p>
+      <DropdownToggle caret>
+        APIs
+        </DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem header>Node.js Based APIs</DropdownItem>
+        <a href='https://uselessapi.com/api/name-gen'><DropdownItem>Name Generator 2</DropdownItem></a>
+        <DropdownItem divider />
+        <DropdownItem header>Python Based APIs</DropdownItem>
+        <DropdownItem> Sorting Algorithms</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+        </Container>
+      </Jumbotron>
     </div>
   );
-}
+};
 
-export default App;
+export default Example;
